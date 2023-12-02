9875433a71ae4540b33c8b1a256ccc797a7ae5a5
Reyna <reynafox@zohomail.eu>
2023-12-02 12:41:57 +0100

    Day 1 & 2.

---
+++ Day1/Program.cs
@@ -0,0 +1,54 @@
+﻿part1();
+part2();
+
+static void part1() {
+	var digits = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	var sum = 0;
+	foreach (var line in File.ReadLines("input.txt")) {
+		var first = line[line.IndexOfAny(digits)].ToString();
+		var last = line[line.LastIndexOfAny(digits)].ToString();
+		var num = int.Parse(first+last);
+		sum += num;
+	}
+
+	Console.WriteLine(sum);
+}
+
+static void part2() {
+	var digits = new[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+	var words = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+	var sum = 0;
+	foreach (var line in File.ReadLines("input.txt")) {
+		var firstPos = int.MaxValue;
+		var firstDigit = 0;
+		var lastPos = -1;
+		var lastDigit = 0;
+		for (int i = 0; i < 10; i++) {
+			newFirstPos(line.IndexOf(digits[i]), i, ref firstPos, ref firstDigit);
+			newFirstPos(line.IndexOf(words[i]), i, ref firstPos, ref firstDigit);
+
+			newLastPos(line.LastIndexOf(digits[i]), i, ref lastPos, ref lastDigit);
+			newLastPos(line.LastIndexOf(words[i]), i, ref lastPos, ref lastDigit);
+		}
+
+		var num = firstDigit*10+lastDigit;
+		sum += num;
+	}
+
+	Console.WriteLine(sum);
+}
+
+static void newFirstPos(int foundPos, int digit, ref int firstPos, ref int firstDigit) {
+	if (foundPos != -1 && foundPos < firstPos) {
+		firstPos = foundPos;
+		firstDigit = digit;
+	}
+}
+static void newLastPos(int foundPos, int digit, ref int lastPos, ref int lastDigit) {
+	if (foundPos != -1 && foundPos > lastPos) {
+		lastPos = foundPos;
+		lastDigit = digit;
+	}
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,64 @@
+﻿part1();
+part2();
+
+static void part1() {
+	var sum = 0;
+	foreach (var line in File.ReadLines("input.txt")) {
+		if (string.IsNullOrEmpty(line)) continue;
+		var parts = line.Split(':');
+		var id = int.Parse(parts[0].Substring(parts[0].LastIndexOf(' ')+1));
+
+		const int validRed = 12;
+		const int validGreen = 13;
+		const int validBlue = 14;
+
+		var draws = parts[1].Split(';', StringSplitOptions.RemoveEmptyEntries);
+		var valid = true;
+		foreach (var draw in draws) {
+			var cubes = draw.Split(',', StringSplitOptions.RemoveEmptyEntries);
+			foreach (var cube in cubes) {
+				parts = cube.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+				var num = int.Parse(parts[0]);
+				var color = parts[1];
+				if (color == "red" && num > validRed) valid = false;
+				if (color == "green" && num > validGreen) valid = false;
+				if (color == "blue" && num > validBlue) valid = false;
+			}
+		}
+
+		if (valid) sum += id;
+	}
+
+	Console.WriteLine(sum);
+}
+
+static void part2() {
+	var sum = 0;
+	foreach (var line in File.ReadLines("input.txt")) {
+		if (string.IsNullOrEmpty(line)) continue;
+		var parts = line.Split(':');
+		var id = int.Parse(parts[0].Substring(parts[0].LastIndexOf(' ')+1));
+
+		var reqdRed = 0;
+		var reqdGreen = 0;
+		var reqdBlue = 0;
+
+		var draws = parts[1].Split(';', StringSplitOptions.RemoveEmptyEntries);
+		var valid = true;
+		foreach (var draw in draws) {
+			var cubes = draw.Split(',', StringSplitOptions.RemoveEmptyEntries);
+			foreach (var cube in cubes) {
+				parts = cube.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+				var num = int.Parse(parts[0]);
+				var color = parts[1];
+				if (color == "red") reqdRed = Math.Max(reqdRed, num);
+				if (color == "green") reqdGreen = Math.Max(reqdGreen, num);
+				if (color == "blue") reqdBlue = Math.Max(reqdBlue, num);
+			}
+		}
+
+		sum += reqdRed*reqdBlue*reqdGreen;
+	}
+
+	Console.WriteLine(sum);
+}
