23bbe479cae678bb0dedbd67e89601ba3f048126
Reyna <reynafox@zohomail.eu>
2023-12-20 19:57:40 +0100

    Day 18 & 19.

---
+++ Day18/Program.cs
@@ -0,0 +1,157 @@
+﻿using System.Globalization;
+
+part1();
+part2();
+
+static void part1() {
+	var actions = new List<(Direction direction, int steps)>();
+	var p = new Vec2(0, 0);
+	var min = new Vec2(0, 0);
+	var max = new Vec2(0, 0);
+
+	var directions = new Dictionary<Direction, Vec2> {
+		{ Direction.up, new Vec2( 0, -1) },
+		{ Direction.down, new Vec2( 0,  1) },
+		{ Direction.right,  new Vec2( 1,  0) },
+		{ Direction.left,  new Vec2(-1,  0) },
+	};
+
+	foreach (var line in File.ReadLines("example.txt")) {
+		var dir = line[0] switch {
+			'U' => Direction.up,
+			'R' => Direction.right,
+			'D' => Direction.down,
+			'L' => Direction.left,
+		};
+		var steps = int.Parse(line[2..line.IndexOf('(')]);
+		actions.Add((dir, steps));
+
+		// Track the path already to compute the bounding box
+		p += directions[dir]*steps;
+		min.x = Math.Min(min.x, p.x);
+		min.y = Math.Min(min.y, p.y);
+		max.x = Math.Max(max.x, p.x);
+		max.y = Math.Max(max.y, p.y);
+	}
+
+	var map = new int[max.x-min.x+1, max.y-min.y+2]; // We make the map one row taller for simplification later on
+	p = new Vec2(-min.x, -min.y);
+	map[p.x, p.y] = 1;
+
+	// Trace path again, paint in the path
+	foreach (var (dir, steps)in actions) {
+		var d = directions[dir];
+		for (int i = 0; i < steps; i++) {
+			p += d;
+			map[p.x, p.y] = 1;
+		}
+	}
+	/*
+	for (int y = 0; y < map.GetLength(1); y++) {
+		var s = "";
+		for (int x = 0; x < map.GetLength(0); x++) {
+			s += map[x, y] == 0 ? '.' : '#';
+		}
+		Console.WriteLine(s);
+	}
+	*/
+
+	// In-out algorithm seems to be safe for the input, there are no crossing sections in the
+	// boundary.
+	var count = 0;
+	for (int y = 0; y < map.GetLength(1); y++) {
+		var inside = false;
+		for (int x = 0; x < map.GetLength(0); x++) {
+			var tile = map[x, y];
+			if (tile != 0) {
+				count++;
+				// Flip inside/outside on corners │, ┌, ┐
+				if (map[x, y+1] != 0) inside = !inside;
+			} else if (inside) {
+				count++;
+			}
+		}
+	}
+
+	Console.WriteLine(count);
+}
+
+static void part2() {
+
+	var directions = new Dictionary<Direction, Vec2> {
+		{ Direction.up, new Vec2( 0, -1) },
+		{ Direction.down, new Vec2( 0,  1) },
+		{ Direction.right,  new Vec2( 1,  0) },
+		{ Direction.left,  new Vec2(-1,  0) },
+	};
+
+	var points = new List<Vec2>();
+	var p = new Vec2(0, 0);
+	var boundaryLength = 0L;
+	foreach (var line in File.ReadLines("input.txt")) {
+		var color = line[(line.IndexOf('(')+2)..line.IndexOf(')')];
+		var steps = int.Parse(color[..5], NumberStyles.HexNumber);
+		var dir = color[5] switch {
+			'0' => Direction.right,
+			'1' => Direction.down,
+			'2' => Direction.left,
+			'3' => Direction.up,
+		};
+
+		boundaryLength += steps;
+		p += directions[dir]*steps;
+		points.Add(p);
+	}
+
+	// Compute polygon area
+	var sum = 0L;
+	var cornersCw = 0;
+	var cornersCcw = 0;
+	var windingSum = 0L;
+	for (int i = 0; i < points.Count; i++) {
+		var p0 = points[(i+points.Count-1)%points.Count];
+		var p1 = points[i];
+		var p2 = points[(i+1)%points.Count];
+
+		var turnsCCW = isLeft(p0, p1, p2);
+		if (turnsCCW) cornersCcw++;
+		else cornersCw++;
+
+		windingSum += (p2.x-p1.x)*(p2.y+p1.y);
+
+		var val = p1.x*p2.y - p2.x*p1.y;
+		sum += val;
+	}
+
+	var windsClockwise = windingSum < 0;
+	if (!windsClockwise) (cornersCcw, cornersCw) = (cornersCw, cornersCcw);
+	var correction = (boundaryLength+(cornersCw-cornersCcw)/2);
+
+	Console.WriteLine($"Winding: {windingSum} Boundary: {boundaryLength} CW {cornersCw} CCW {cornersCcw}");
+	Console.WriteLine($"Correction {boundaryLength+cornersCw-cornersCcw}");
+	Console.WriteLine((sum+correction)/2);
+
+	bool isLeft(Vec2 l1, Vec2 l2, Vec2 p) {
+		return (l2.x-l1.x)*(p.y-l1.y) - (l2.y-l1.y)*(p.x-l1.x) < 0;
+	}
+}
+
+enum Direction {
+	up,
+	right,
+	down,
+	left
+}
+
+class Vec2(long x, long y) {
+	public long x = x;
+	public long y = y;
+
+	public static Vec2 operator +(Vec2 a, Vec2 b) {
+		return new Vec2(a.x+b.x, a.y+b.y);
+	}
+
+	public static Vec2 operator *(Vec2 a, long b) {
+		return new Vec2(a.x*b, a.y*b);
+	}
+}
---
+++ Day19/Program.cs
@@ -0,0 +1,199 @@
+﻿part1();
+part2();
+
+static void part1() {
+	var actions = new Dictionary<string, List<Condition>>();
+	var inItems = false;
+	var sum = 0;
+	foreach (var line in File.ReadLines("input.txt")) {
+		if (string.IsNullOrEmpty(line)) {
+			inItems = true;
+			continue;
+		}
+		if (!inItems) {
+			parseAction(line, actions);
+		} else {
+			var item = new Dictionary<char, int>();
+			var open = line.IndexOf('{');
+			var close = line.IndexOf('}');
+			foreach (var part in line[(open+1)..close].Split(',')) {
+				item[part[0]] = int.Parse(part[2..]);
+			}
+
+			var next = "in";
+			do {
+				var act = actions[next];
+				foreach (var c in act) {
+					if (c.matches(item)) {
+						next = c.next;
+						break;
+					}
+				}
+			} while (next != "R" && next != "A");
+
+			if (next == "A") {
+				var isum = item.Sum(x => x.Value);
+				Console.WriteLine($"Item {line} accepted with value {isum}");
+				sum += item.Sum(x => x.Value);
+			}
+		}
+	}
+
+	Console.WriteLine(sum);
+}
+
+static void part2() {
+	var actions = new Dictionary<string, List<Condition>>();
+	foreach (var line in File.ReadLines("input.txt")) {
+		if (string.IsNullOrEmpty(line)) {
+			break;
+		}
+		parseAction(line, actions);
+	}
+
+	var items = new List<(string next, Item)> {
+		("in", new Item {
+			{ 'x', new Range(1, 4000) },
+			{ 'm', new Range(1, 4000) },
+			{ 'a', new Range(1, 4000) },
+			{ 's', new Range(1, 4000) }
+		})
+	};
+
+
+	var accepted = new List<Item>();
+
+	while (items.Count > 0) {
+		/*
+		foreach (var i in items) {
+			Console.WriteLine(i);
+		}
+		Console.WriteLine("-------------------");
+		*/
+		var (next, item) = items[^1];
+		items.RemoveAt(items.Count-1);
+		var act = actions[next];
+		foreach (var c in act) {
+			var rejected = item.rejectedPart(c);
+			if (!item.isEmpty()) {
+				if (c.next == "A")
+					accepted.Add(item);
+				else if (c.next != "R")
+					items.Add((c.next, item));
+			}
+			if (rejected == null) break;
+			item = rejected;
+		}
+	}
+
+	Console.WriteLine("Accepted");
+	var sum = 0L;
+	foreach (var i in accepted) {
+		Console.WriteLine(i);
+		var count = i['x'].size*i['m'].size*i['a'].size*i['s'].size;
+		sum += count;
+		Console.WriteLine(count);
+	}
+
+	Console.WriteLine(sum);
+}
+
+static void parseAction(string line, Dictionary<string, List<Condition>> actions) {
+	var open = line.IndexOf('{');
+	var close = line.IndexOf('}');
+	var name = line[..open];
+	var conditions = new List<Condition>();
+	foreach (var part in line[(open+1)..close].Split(',')) {
+		conditions.Add(new Condition(part));
+	}
+	actions[name] = conditions;
+}
+
+class Condition {
+	public readonly char var;
+	public readonly char op;
+	public readonly int val;
+	public readonly string next;
+
+	public Condition(string s) {
+		if (s.Length <= 1 || (s[1] != '<' && s[1] != '>')) {
+			var = '*';
+			op = '*';
+			val = 0;
+			next = s;
+		} else {
+			var = s[0];
+			op = s[1];
+			var cPos = s.IndexOf(':');
+			val = int.Parse(s[2..cPos]);
+			next = s[(cPos+1)..];
+		}
+	}
+
+	public bool matches(Dictionary<char, int> item) {
+		switch (op) {
+		case '<': return item[var] < val;
+		case '>': return item[var] > val;
+		case '*': return true;
+		}
+		return true;
+	}
+}
+
+class Range {
+	public int min;
+	public int max;
+
+	public Range(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	public Range(Range other) {
+		this.min = other.min;
+		this.max = other.max;
+	}
+
+	public bool isEmpty() => min>=max;
+	public long size => max-min+1;
+
+	public override string ToString() {
+		return $"Range({min}, {max})";
+	}
+}
+
+class Item: Dictionary<char, Range?> {
+	public Item(): base() { }
+	public Item(Item other): base(other) { }
+
+	public Item? rejectedPart(Condition c) {
+		if (c.op == '*') return null;
+
+		Range rejectedRange;
+		if (c.op == '<') {
+			rejectedRange = new Range(c.val, this[c.var].max);
+			this[c.var].max = c.val-1;
+		} else {
+			rejectedRange = new Range(this[c.var].min, c.val);
+			this[c.var].min = c.val+1;
+		}
+
+		if (this[c.var].isEmpty()) this[c.var] = null;
+
+		var result = new Item() {
+			{ 'x', new Range(this['x']) },
+			{ 'm', new Range(this['m']) },
+			{ 'a', new Range(this['a']) },
+			{ 's', new Range(this['s']) }
+		};
+		result[c.var] = rejectedRange.isEmpty() ? null : rejectedRange;
+
+		return result;
+	}
+
+	public bool isEmpty() => this.Any(x => x.Value == null);
+
+	public override string ToString() {
+		return $"Item({string.Join(", ", this.Select(x => $"[{x.Key}, {x.Value}]"))})";
+	}
+}
